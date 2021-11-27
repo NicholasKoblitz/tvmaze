60ee97914afb02c0a99b44abea95c1b098beee8d
NicholasK <nicholask1@outlook.com>
2021-11-27 13:19:07 -0500

    Finshed development

--- tvmaze.js
+++ tvmaze.js
@@ -17,18 +17,26 @@
         image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
       }
  */
+
+const MISSING_URL = "https://tinyurl.com/tv-missing";
+
 async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  Remove
   // hard coded data.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  const results = await axios.get("https://api.tvmaze.com/search/shows", { params: { q: query } })
+  const shows = [];
+
+  for (let item of results.data) {
+    shows.push({
+      id: item.show.id,
+      name: item.show.name,
+      summary: item.show.summary,
+      image: item.show.image ? item.show.image.medium : MISSING_URL
+    });
+  }
+
+  return shows;
 }
 
 
@@ -42,19 +50,23 @@ function populateShows(shows) {
   $showsList.empty();
 
   for (let show of shows) {
+    // console.log(show.image)
     let $item = $(
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
-         <div class="card" data-show-id="${show.id}">
-           <div class="card-body">
-             <h5 class="card-title">${show.name}</h5>
-             <p class="card-text">${show.summary}</p>
-           </div>
-         </div>
-       </div>
+        <div class="card" data-show-id="${show.id}">
+          <img class="card-img-top" src="${show.image}">
+            <div class="card-body">
+              <h5 class="card-title">${show.name}</h5>
+              <p class="card-text">${show.summary}</p>
+              <button type="button" id="${show.id}">Episodes</button>
+          </div>
+        </div>
+      </div>
       `);
 
     $showsList.append($item);
   }
+
 }
 
 
@@ -63,7 +75,7 @@ function populateShows(shows) {
  *    - get list of matching shows and show in shows list
  */
 
-$("#search-form").on("submit", async function handleSearch (evt) {
+$("#search-form").on("submit", async function handleSearch(evt) {
   evt.preventDefault();
 
   let query = $("#search-query").val();
@@ -73,7 +85,25 @@ $("#search-form").on("submit", async function handleSearch (evt) {
 
   let shows = await searchShows(query);
 
+
   populateShows(shows);
+
+  for (let show of shows) {
+
+    $(`#${show.id}`).on('click', async function displayEpisodes(e) {
+      const id = show.id;
+
+      const episodes = await getEpisodes(id);
+
+      $('#episodes-area').show();
+
+      populateEpisode(episodes);
+
+    })
+
+  }
+
+
 });
 
 
@@ -86,5 +116,37 @@ async function getEpisodes(id) {
   //       you can get this by making GET request to
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
 
+  const episResults = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`)
+
   // TODO: return array-of-episode-info, as described in docstring above
+
+  const episList = [];
+
+  for (let episode of episResults.data) {
+    episList.push({
+      id: episode.id,
+      name: episode.name,
+      season: episode.season,
+      number: episode.number
+    });
+  }
+
+  return episList
+
+
 }
+
+// Places the list of episodes on the DOM
+function populateEpisode(episodes) {
+  const $episodeList = $("#episodes-list");
+  $episodeList.empty();
+
+  // Creates li elements for episodes and appends to the DOM
+  for (let episode of episodes) {
+    let $episode = $(`<li><strong>${episode.name}</strong>: (Season:${episode.season}, Episode:${episode.number})</li>`);
+    $episodeList.append($episode)
+  }
+
+}
+
+
